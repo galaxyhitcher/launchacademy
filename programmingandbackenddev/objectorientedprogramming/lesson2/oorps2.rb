9553f735c88df5638392836fae8d5909bde7fa6c
Louis Heimel <louisheimel@Louiss-MBP.lan>
2015-12-06 19:08:33 -0600

    finish second iteration of obj-oriented rps

--- programmingandbackenddev/objectorientedprogramming/lesson2/oorps2.rb
+++ programmingandbackenddev/objectorientedprogramming/lesson2/oorps2.rb
@@ -1,6 +1,6 @@
 class Player
   attr_accessor :move, :name
-
+  
   def initialize
     set_name
   end
@@ -19,7 +19,14 @@ class Human < Player
   end
 
   def choose
-    self.move = ['rock', 'paper', 'scissors'].sample
+    choice = nil
+    loop do
+      puts "Please choose rock, paper, or scissors."
+      choice = gets.chomp
+      break if Move::VALUES.include? choice
+      puts "Sorry, invalid choice."
+    end
+    self.move = Move.new(choice)
   end
 end
 
@@ -29,21 +36,58 @@ class Computer < Player
   end
 
   def choose
-    choice = nil
-    loop do
-      puts "Please choose rock, paper, or scissors:"
-      choice = gets.chomp
-      break if ['rock', 'paper', 'scissors'].include? choice
-      puts "Sorry, invalid choice."
-    end
-    self.move = choice
+    self.move = Move.new(Move::VALUES.sample)
   end
 end
 
 class Move
-  def initialize
-    # seems like we need something to keep track
-    # of the choice... a move object can be "paper", "rock" or "scissors"
+  VALUES = ['rock', 'paper', 'scissors']
+
+  def initialize(value)
+    raise ArgumentError.new if !VALUES.include?(value)
+    @value = value
+  end
+
+  def scissors?
+    @value == 'scissors'
+  end
+
+  def rock?
+    @value == 'rock?'
+  end
+
+  def paper?
+    @value == 'paper'
+  end
+
+  def >(other_move)
+    if rock?
+      return true if other_move.scissors?
+      return false
+    elsif paper?
+      return true if other_move.rock?
+      return false
+    elsif scissors?
+      return true if other_move.paper?
+      return false
+    end
+  end
+
+  def <(other_move)
+    if rock?
+      return true if other_move.paper?
+      return false
+    elsif paper?
+      return true if other_move.scissors?
+      return false
+    elsif scissors?
+      return true if other_move.rock?
+      return false
+    end
+  end
+
+  def to_s
+    @value
   end
 end
 
@@ -81,19 +125,12 @@ class RPSGame
     puts "#{human.name} chose #{human.move}"
     puts "#{computer.name} chose #{computer.move}"
 
-    case human.move
-    when 'rock'
-      puts "It's a tie!" if computer.move == 'rock'
-      puts "#{human.name} won!" if computer.move == 'scissors'
-      puts "#{computer.name} won!" if computer.move == 'paper'
-    when 'paper'
-      puts "It's a tie!" if computer.move == 'paper'
-      puts "#{human.name} won!" if computer.move == 'rock'
-      puts "#{computer.name} won!" if computer.move == 'scissors'
-    when 'scissors'
-      puts "It's a tie!" if computer.move == 'scissors'
-      puts "#{human.name} won!" if computer.move == 'paper'
-      puts "#{computer.name} won!" if computer.move == 'rock'
+    if human.move > computer.move
+      puts "#{human.name} won!"
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
     end
   end
 
