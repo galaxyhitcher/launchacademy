c081b6ff978f6e997a6cc90089cf0f962421946a
Louis Heimel <louisheimel@Louiss-MacBook-Pro.local>
2016-03-06 01:14:55 -0600

    adding login and logout

--- programmingandbackenddev/webdevelopment/cms/cms.rb
+++ programmingandbackenddev/webdevelopment/cms/cms.rb
@@ -101,5 +101,26 @@ post "/:filename" do
   redirect "/"
 end
 
+get "/users/signin" do
+  erb :signin
+end
+
+post "/users/signin" do
+  if params[:username] == "admin" && params[:password] == "secret"
+    session[:username] = params[:username]
+    session[:message] = "Welcome!"
+    redirect "/"
+  else
+    session[:message] = "Invalid credentials"
+    status 422
+    erb :signin
+  end
+end
+
+post "/users/signout" do
+  session.delete(:username)
+  session[:message] = "You have been signed out."
+  redirect "/"
+end
 
 
