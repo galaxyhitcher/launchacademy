5119757d3cf5791b512c9a4c02c54dc7932c856f
louisheimel <louisheimel1@gmail.com>
2016-10-07 13:47:32 -0500

    finish dynamic animations project

---
+++ frontenddevelopment/frontenddevwithjs/Dynamic Animations/javascripts/animations.js
@@ -0,0 +1,67 @@
+$(function() {
+  var $canvas = $("#shape_container");
+
+  function getFormObject($f) {
+    var o = {};
+
+    $f.serializeArray().forEach(function(input) {
+      o[input.name] = input.value;
+    });
+
+    return o;
+  }
+
+  function createElement(data) {
+    var $d = $("<div />", {
+      "class": data.shape,
+      data: data,
+    });
+
+    resetElement($d);
+
+    return $d;
+  }
+
+  function animateElement() {
+    var $e = $(this),
+        data = $e.data();
+    resetElement($e);
+    $e.animate({
+      left: +data.x_end_coord,
+      top: +data.y_end_coord,
+    }, 1000);
+  }
+
+  function resetElement($e) {
+    var data = $e.data();
+    $e.css({
+      left: +data.x_start_coord,
+      top: +data.y_start_coord,
+    });
+  }
+
+  function stopAnimations() {
+    $canvas.find("div").stop();
+  }
+
+  $('form').on('submit', function(e) {
+    e.preventDefault();
+
+    var $f = $(this),
+        data = getFormObject($f);
+
+    $canvas.append(createElement(data));
+  });
+
+  $('#start').on('click', function(e) {
+    e.preventDefault();
+
+    $canvas.find("div").each(animateElement);
+  });
+
+  $("#stop").on("click", function(e) {
+    e.preventDefault();
+
+    stopAnimations();
+  });
+});
