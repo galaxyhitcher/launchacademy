ca88b6689860cfead74712b51f520ff385eef079
louisheimel <louisheimel1@gmail.com>
2016-10-08 14:25:49 -0500

    add first draft of inventory order project

--- frontenddevelopment/frontenddevwithjs/Inventory Orders/inventory.js
+++ frontenddevelopment/frontenddevwithjs/Inventory Orders/inventory.js
@@ -10,9 +10,37 @@ var inventory;
     getCachedElement: function() {
       this.template = $("#inventory_item").remove().html();
     },
+    incrementedCount: 1,
+    addItem: function() {
+      var that = this;
+      $("#inventory").append(that.template.replace(/ID/g, that.incrementedCount));
+      this.collection.push({
+        id: that.incrementedCount,
+        name: "",
+        stockNumber: "",
+        quantity: 1,
+      });
+      this.incrementedCount++;
+    },
+    deleteRowAt: function(i) {
+      this.collection.splice(i - 1, 1);
+      $('table tr').eq(i - 1).remove();
+    },
     init: function() {
+      var that = this;
       this.getCachedElement();
       this.setDate();
+      $("#add_item").on("click", function() {
+        that.addItem();
+      });
+
+
+      $("#inventory").on("click", "a", function() {
+        var current_index = $(this).closest('tr').index() + 1;
+        that.deleteRowAt(current_index);
+      })
+
+
     }
   };
 
