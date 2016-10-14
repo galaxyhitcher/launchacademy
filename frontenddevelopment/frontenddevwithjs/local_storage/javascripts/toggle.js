ffaccaae5c7e19f21107271fd0cd2cd91e0cdecd
louisheimel <louisheimel1@gmail.com>
2016-10-14 13:18:35 -0500

    complete local storage project

---
+++ frontenddevelopment/frontenddevwithjs/local_storage/javascripts/toggle.js
@@ -0,0 +1,49 @@
+$(function() {
+  if (localStorage.activeNav) {
+    $('.active').removeClass('active');
+    $('a').eq(localStorage.activeNav).addClass('active');
+    toggleArticle(localStorage.activeNav);
+  }
+
+  if (localStorage.backgroundColor) {
+    $('body, main').css({
+      'background-color': localStorage.backgroundColor,
+    });
+  }
+
+  if (localStorage.note) {
+    $('textarea').text(localStorage.note);
+  }
+
+  $('li').change(function() {
+    var backgroundColor = $(this).find('input').val();
+    $('body, main').css({
+      'background-color': backgroundColor,
+    });
+    localStorage.setItem('backgroundColor', backgroundColor);
+  })
+
+  $('a').click(function(e) {
+    e.preventDefault();
+    $('.active').removeClass('active');
+    $(e.target).addClass('active');
+    clickedIndex = $('a').index(this);
+    toggleArticle(clickedIndex);
+  });
+
+  function toggleArticle(justClickedIndex) {
+    localStorage.setItem('activeNav', justClickedIndex);
+    $('article:visible').css({
+      display: 'none',
+    });
+
+    $('article').eq(justClickedIndex).css({
+      display: 'block',
+    });
+  }
+});
+
+$(window).on("unload", function() {
+  var note = $('textarea').val();
+  localStorage.setItem('note', note);
+});
