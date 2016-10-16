14da29d82bfdc1f68aa49a7b2f916792dfd56d2c
louisheimel <louisheimel1@gmail.com>
2016-10-16 16:18:25 -0500

    begin image manipulator demo

---
+++ frontenddevelopment/frontenddevwithjs/image_manipulator/javascripts/site.js
@@ -0,0 +1,15 @@
+$(window).on("load", function() {
+  console.log('hello');
+  var ctx;
+  $("<canvas></canvas>").appendTo("body");
+  ctx = $("canvas")[0].getContext('2d');
+
+  var manipulator = {
+    bind: function() {
+
+    },
+    init: function() {
+
+    },
+  }
+});
